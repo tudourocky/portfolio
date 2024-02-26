0f95110dbf4c1bf4943e35b8ae978e72aecd0753
sucarrie2 <tudourocky@gmail.com>
2024-02-25 21:59:58 -0500

    setting up github pages

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,52 +4,58 @@ import Logo from "../../assets/logo.svg";
 
 const Sidebar = () => {
     return(
-        <aside className="aside">
-            <a href="#home" className="nav_logo">
-                <img src={Logo} alt="" width="500px" height="auto"/>
-            </a>
+        <>
+            <aside className="aside">
+                <a href="#home" className="nav_logo">
+                    <img src={Logo} alt="" width="500px" height="auto"/>
+                </a>
 
-            <nav className="nav">
-                <div className="nav__menu">
-                    <ul className="nav__list">
-                        <li className="nav__item">
-                            <a href="#home" className="nav__link">
-                                <i className="icon-home"></i>
-                            </a>
-                        </li>
+                <nav className="nav">
+                    <div className="nav__menu">
+                        <ul className="nav__list">
+                            <li className="nav__item">
+                                <a href="#home" className="nav__link">
+                                    <i className="icon-home"></i>
+                                </a>
+                            </li>
 
-                        <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <i className="icon-user-following"></i>
-                            </a>
-                        </li>
-                        
-                        
-                        <li className="nav__item">
-                            <a href="#resume" className="nav__link">
-                                <i className="icon-graduation"></i>
-                            </a>
-                        </li>
-                        
-                        <li className="nav__item">
-                            <a href="#portfolio" className="nav__link">
-                                <i className="icon-layers"></i>
-                            </a>
-                        </li>
+                            <li className="nav__item">
+                                <a href="#about" className="nav__link">
+                                    <i className="icon-user-following"></i>
+                                </a>
+                            </li>
+                            
+                            
+                            <li className="nav__item">
+                                <a href="#resume" className="nav__link">
+                                    <i className="icon-graduation"></i>
+                                </a>
+                            </li>
+                            
+                            <li className="nav__item">
+                                <a href="#portfolio" className="nav__link">
+                                    <i className="icon-layers"></i>
+                                </a>
+                            </li>
 
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <i className="icon-bubble"></i>
-                            </a>
-                        </li>
-                    </ul>
+                            <li className="nav__item">
+                                <a href="#contact" className="nav__link">
+                                    <i className="icon-bubble"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div className="nav__footer">
+                    <span className="copyright">&copy; 2024 Portfolio</span>
                 </div>
-            </nav>
+            </aside>
 
-            <div className="nav__footer">
-                <span className="copyright">&copy; 2024 Portfolio</span>
+            <div className="nav__toggle">
+                <i className="icon-menu"></i>
             </div>
-        </aside>
+        </>
     )
 }
 
